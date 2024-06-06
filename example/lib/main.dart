bc3c6ca978458d34fe2e5fa2f7179d285263c9e3
edudin <e.dudin@vk.team>
2024-06-06 10:51:03 +0300

    PI-324: Добавлены кнопки запроса и показа ревью в русторе.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rustore_review/flutter_rustore_review.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,17 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
+}
+
+class SnackbarGlobal {
+  static GlobalKey<ScaffoldMessengerState> key =
+      GlobalKey<ScaffoldMessengerState>();
+
+  static void show(String message) {
+    key.currentState!
+      ..hideCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text(message)));
+  }
 }
 
 class _MyAppState extends State<MyApp> {
@@ -25,25 +37,60 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    launchRuStore() async {
+      const url = "rustore://apps.rustore.ru/app/ru.rustore.reviewflutter";
+
+      if (await canLaunchUrl(Uri.parse(url))) {
+        await launchUrl(Uri.parse(url));
+      } else {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('RuStore not installed!')));
+        }
+      }
+    }
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-            child: OutlinedButton(
-          onPressed: () {
-            RustoreReviewClient.request().then((value) {
-              RustoreReviewClient.review().then((value) {
-                print("success review");
-              }, onError: (err) {
-                print("on err ${err}");
-              });
-            });
-          },
-          child: Text('Review'),
-        )),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
       ),
-    );
+      body: Builder(
+          builder: (BuildContext context) => Center(
+                child: Column(children: [
+                  OutlinedButton(
+                    child: const Text('Request Review'),
+                    onPressed: () {
+                      RustoreReviewClient.request().then((value) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    'ReviewInfo requested successfully!')));
+                      }, onError: (err) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    'ReviewInfo request error! See log!')));
+                      });
+                    },
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      RustoreReviewClient.review().then((value) {
+                        debugPrint("success review");
+                      }, onError: (err) {
+                        debugPrint("on err ${err}");
+                      });
+                    },
+                    child: const Text('Review'),
+                  ),
+                  OutlinedButton(
+                      onPressed: () {
+                        launchRuStore();
+                      },
+                      child: const Text('Open review in RuStore'))
+                ]),
+              )),
+    ));
   }
 }
